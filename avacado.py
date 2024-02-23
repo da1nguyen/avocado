0fd0e8e51bd40dd22d12dfd60aec14427e9f8f3b
Thon-Da Nguyen <67877012+da1nguyen@users.noreply.github.com>
2024-02-23 17:14:48 +0700

    Update avacado.py

--- avacado.py
+++ avacado.py
@@ -1,15 +1,11 @@
+# Import python libraries
 import streamlit as st
+import seaborn as sns
 import pandas as pd
-import plotly.express as px
+import matplotlib.pyplot as plt
 # Data Set
-data = pd.read_csv("avocado_full.csv")
-# Minimizing Dataset
-albany_df = data[data['region']=="Albany"]
-al_df = albany_df[albany_df["year"]==2015]
-#Line
-line_chart = px.line(
-x = al_df["Date"],
-y = al_df["Large Bags"]
-)
-st.header("Line Chart")
-st.plotly_chart(line_chart)
+df = pd.read_csv("avocado_full.csv")
+# Defining Count Graph/Plot
+fig = plt.figure(figsize=(10, 5))
+sns.countplot(x = "year", data = df)
+st.pyplot(fig)
